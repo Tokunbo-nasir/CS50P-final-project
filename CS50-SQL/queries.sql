51c759566ef86a15a5f175e3fed57f3504c2606d
Tokunbo-nasir <nastv97@gmail.com>
2024-08-03 15:45:37 +0100

    Added common queries to queries.sql file

--- CS50-SQL/queries.sql
+++ CS50-SQL/queries.sql
@@ -1,2 +1,9 @@
 --TODO which is an annotated set of SELECT, INSERT, UPDATE, DELETE, etc. 
 --statements that users will commonly run on your database.
+
+INSERT INTO bodyspec(bodytype, doors, gearbox) VALUES(SUV,5,Automatic);
+INSERT INTO enginespec(enginesize, fueltype, horsepower) VALUES(6.0, petrol, 154);
+INSERT INTO carspec(make, reg, year, price, milage) VALUES(BMW, 23, 2023, 19000, 40000);
+
+UPDATE `carspec` JOIN `bodyspec` ON `bodyspec`.`id`=`carspec`.`id` SET `carspec`.`body_id` = `bodyspec`.`id`;
+UPDATE `carspec` JOIN `enginespec` ON `enginespec`.`id`=`carspec`.`id` SET `carspec`.`engine_id` = `enginespec`.`id`;
